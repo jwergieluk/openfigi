ff4e769102295280b9e5ad703c5b676f399df894
Julian Wergieluk <julian@wergieluk.com>
2017-02-19 23:06:36 +0100

    Add a basic unit test

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,10 +1,19 @@
 import unittest
+import openfigi
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_wkn_ticker_anonymous(self):
+        """Get an ETF by WKN and check if response makes sense"""
+        ofg = openfigi.OpenFigi()
+        ofg.enqueue_request(id_type='ID_WERTPAPIER', id_value='A0YEDG')
 
+        response = ofg.fetch_response()
+        self.assertTrue(type(response) is list)
+        self.assertTrue(len(response) > 0)
+        self.assertTrue(type(response[0]) is dict)
+        self.assertTrue('data' in response[0].keys())
+        self.assertTrue(len(response[0]['data']) > 0)
 
 if __name__ == '__main__':
     unittest.main()
